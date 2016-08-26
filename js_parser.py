92c09f0f8796da6ba9c5b46e3e41dfcc9792119c
장지수 <rilize0536@gmail.com>
2016-08-26 15:10:44 +0900

    backup_crawler

--- js_parser.py
+++ js_parser.py
@@ -29,7 +29,7 @@ class cparser:
             soup = BeautifulSoup(html,"html.parser")
             return soup.find_all(self.tag)
         except:
-            print("exception error")
+            print("exception error" + self.url)
             exit()
 
     def getAttrs(self,arr): #원하는 속성 파싱후 중복제거한 리스트 리턴
@@ -39,14 +39,15 @@ class cparser:
         arrAttr = list(set(arrAttr))
         return arrAttr
 
-    def getPatternAttrs(self,arr,*args): # 원하는 속성 패턴검색해서 파싱후 중복제거한 리스트 리턴
+    def getPatternAttrs(self,arr,*args): # 원하는 속성 패턴검색해서 파싱후 중복제거한 리스트 리턴 패턴은 원하는 만큼 설정 가능
         argsSize = len(args)
         import re
         arrAttr = []
-        for attr in arr:
-            for i in args:
+        for i in args: #속성 검사 시작
+            for attr in arr:
                 if re.match(i,attr[self.attr]):
                     arrAttr.append(attr[self.attr])
+        #arrAttr.sort()
         arrAttr = list(set(arrAttr))
         return arrAttr
 
@@ -59,14 +60,32 @@ class cparser:
     def setTag(self,tag):
         self.tag = tag
 
+    #def searchAll(self,):
+
 
 if __name__ == "__main__":
-    parse = cparser("http://220.86.25.65:9999","a","href")
-    tags = []
-    attrs = []
-    temp = []
-    tags = parse.getTags()
-    temp = tags
-    attrs = parse.getPatternAttrs(tags,"[/]","[./]","[../]","[ ]")
-    temp = attrs
-    print(temp)
+    counter = 0 # 검사한 url 갯수
+    checkedUrl = [] # 체크 완료한 url
+    tags = [] #뽑아낸 태그들
+    attrs = [] # 뽑아낸 속성들
+    temp = [] #임시 저장소
+    url = "http://220.86.25.65:9999"
+    result = 0 #
+    count = 3
+    while result < count:
+        attr = "/main.asp"
+        if 0 < len(attrs):
+            attr = attrs[result]
+        if attr[0:1]==".":
+            attr = attr.replace("./","/")
+
+        parse = cparser(url+attr,"a","href")
+        print(attr)
+        tags = parse.getTags()
+
+        attrs = parse.getPatternAttrs(tags,"[/]","[./]","[../]","[ ]")
+        temp = attrs+temp
+        temp = list(set(temp))
+        print(temp)
+        result = result+1
+        count = len(temp)
