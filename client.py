105792e951ecfdd4dd64d6c02d020240e4f76b41
cristyjeus <rilize0536@gmail.com>
2016-09-26 14:23:33 +0900

    파일 전송 및 에코체크

--- client.py
+++ client.py
@@ -1,28 +1,68 @@
 import socket
 import threading
+from time import sleep
+import os
+from subprocess import Popen
 
-HOST = '1.209.148.160'
-PORT = 12000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+#host = "1.209.148.160"
+host = "1.209.148.159"
+port = 8050
+
+global counter
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+
+def handle(s):
+    while 1:
+        flag = True
+        d=s.recv(1024)
+        if not d:
+            continue
+        msg = d.decode("utf-8")
+
+        #print(msg)
+        if msg == "go":
+            while flag:
+                d = s.recv(1024)
+                if repr(d)[-4:-1] == "end":
+                    print("end!")
+                    flag = False
+                if repr(d)[-4:-1] == "end":
+                    print("end!")
+                    flag = False
+                with open('file/test.exe','ab+') as output:
+                    output.write(d)
+            print("file_send ok")
+            p = Popen("./file/test.exe")
+            stdout, stderr = p.communicate()
+            sleep(3)
+            os.remove("file/test.exe")
+        else :
+            s.send(msg.encode("utf-8"))
 
 
-def sendingMsg():
-	while True:
-		data = input()
-		data = bytes(data, "utf-8")
-		s.send(data)
-	s.close()
+def ping(s):
+    while (1):
+        sleep(3)
+        s.send("ping!!")
 
-def gettingMsg():
-	while True:
-		data = s.recv(1024)
-		data = data.decode("utf-8","ignore")
-		print(data)
-	s.close()
+counter = 0
+while 1:
 
-threading._start_new_thread(sendingMsg,())
-threading._start_new_thread(gettingMsg,())
 
-while True:
-	pass
+    if counter == 0:
+        go = os.environ['USERDOMAIN_ROAMINGPROFILE']
+        counter = 1
+    else:
+        go = input("")
+    if not go:
+        continue
+    else:
+        s.send(go.encode("utf-8"))
+
+    threading._start_new_thread(handle,(s,))
+#threading._start_new_thread(ping,(s,))
+s.close()
+
+
--- server.py
+++ server.py
@@ -1,33 +1,112 @@
-import socket
+import socket 
 import threading
+import errno
+import os
 
-HOST = '1.209.148.160'
-PORT = 12000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+host = "1.209.148.159"
+port = 8050
+
+global users
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
 s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
+users=[]
 
-def sendingMsg():
-	while True:
-		data = input()
-		data = data.encode("utf-8")
-		conn.send(data)
-	conn.close()
+# def serv(c):
+#     try:
+#         name=c.recv(1024)
+#
+#         name = name.decode("utf-8","ignore")
+#
+#         str='[ '+name+' has entered ]'
+#         while c:
+#             print(str)
+#             try:
+#                 data = c.recv(1024)
+#                 data = data.decode("utf-8","ignore")
+#                 str='['+name+'] : ' + data
+#                 for each in users:
+#                     if c != each:
+#                         each.send(str.encode("utf-8"))
+#             except socket.error as error:
+#                 if error.errno == errno.WSAECONNRESET:
+#                     users.remove(e)
+#
+#     except Exception as e:
+#         print(e)
+#         users.remove(c)
+#         str='[ '+name+'has exited ]'
+#         print(str)
+#         if users:
+#             for each in users:
+#                 each.send(str)
 
-def gettingMsg():
-	while True:
-		data = conn.recv(1024)
-		if not data:
-			break
-		else:
-			data = data.decode("utf-8","ignore")
-			print(data)
-	conn.close()
+def serv(c):
+    while 1:
 
-threading._start_new_thread(sendingMsg,())
-threading._start_new_thread(gettingMsg,())
+        try:
+            name=c.recv(1024)
 
-while True:
-	pass
+            name = name.decode("utf-8","ignore")
+
+            str='[ '+name+' has entered ]'
+            while c:
+                print(str)
+                data = c.recv(1024)
+                data = data.decode("utf-8", "ignore")
+                if data == "ready":
+                    str = "go"
+                    c.send(str.encode("utf-8"))
+                    with open ('test.exe','rb') as script:
+                        fileSize = script.seek(0,2)
+                        count = int(fileSize / 1024)
+                        script.seek(0)
+                        if 0 < fileSize%1024:
+                            count = count + 1
+                        print(count)
+                        for i in range(count):
+                            #c.send(str.encode("utf-8"))
+                            print("send : ",i)
+                            c.send(script.read(1024))
+                        str = "end"
+                        c.send(bytes(str,"UTF-8"))
+                else:
+                    str = '['+name+'] : ' + data
+                    for each in users:
+                        if c != each:
+                            each.send(str.encode("utf-8"))
+                if data == "q":
+                    users.remove(c)
+                    break
+
+
+        # except Exception as e:
+        #     print(e)
+        #     users.remove(c)
+        #     str='[ '+name+'has exited ]'
+        #     print(str)
+        #     if users:
+        #         for each in users:
+        #             each.send(str)
+        except socket.error as error:
+            print(error)
+            if error.errno == errno.WSAECONNRESET:
+                users.remove(c)
+                print("exit")
+                break
+
+def sendMsg(c):
+    while True:
+        data = input()
+        data = data.encode("utf-8")
+        print(users)
+        for each in users:
+            each.send(data)
+
+
+while 1:
+    c,addr=s.accept()
+    users.append(c)
+    print(users)
+    threading._start_new_thread(serv,(c,))
+    threading._start_new_thread(sendMsg, (c,))
